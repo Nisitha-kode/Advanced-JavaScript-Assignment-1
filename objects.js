c2c4e32e9f8fa0c9e519835544fb202e89ef7618
Nisitha-kode <107039273+Nisitha-kode@users.noreply.github.com>
2022-10-06 19:39:41 +0200

    Add files via upload

---
+++ objects.js
@@ -0,0 +1,12 @@
+//Reference types
+//1.Objects
+//2.Arrays
+//3.Functions
+
+let person = {
+  ///Object declartion
+  name: 'Rithik', //Value pairs with property 1
+  age: 6 //Value pairs with property 2
+}
+person.age = 8 //Acessing object with property 2
+console.log(person.age)
